e247affcb6d154d3f26c06fdf4aeea3d50089c14
Arthur Correia <115330684+artucorreia@users.noreply.github.com>
2022-11-21 12:55:52 -0300

    update
    Já consigo pegar a task específica que o usuário quer checar, editar ou deletar

--- js/script.js
+++ js/script.js
@@ -1,38 +1,55 @@
-let aux = 0;
-const gambiarra = () => {
-    let i = 'id';
-    aux++;
-    return i + aux;
+const check = () => {
+    console.log('check ok')
 }
 
-const selectTask = () => {
-    console.log('ok');
+const edit = () => {
+    console.log('edit ok')
 }
 
-const check = () => selectTask();
+const exclude = () => {
+    console.log('exclude ok')
+}
 
-const edit = () => selectTask();
-
-const exclude = () => selectTask();
-
-// check, edit, delete
-const optionId = {
-    'check': () => check(),
-    'edit': () => edit(),
-    'delete': () => exclude()
+// direciona para check, edit, exclude
+const optionClass = {
+    'fas fa-check': () => check(),
+    'fas fa-pencil': () => edit(),
+    'fas fa-trash': () => exclude(),
+    'btn btn-secondary check': () => check(),
+    'btn btn-secondary edit': () => edit(),
+    'btn btn-secondary delete': () => exclude()
 };
 
-const options = event => optionId[event.target.id]();
+// pega a task que vai ser verificada, editada ou deletada
+const selectTask = taskId => {
+    let task = window.document.getElementById('task' + taskId);
+    console.log(task)
+}
+
+// pega tanto a função que o usuário quer executar 
+let taskId = null;
+const options = event => {
+    taskId = event.target;
+    selectTask(taskId);
+    optionClass[event.target.className]()
+};
 
 const buttons = btns => btns.addEventListener('click', options);
 
 const clearInput = txt => txt.value = '';
 
+// cria id novos
+let aux = 0;
+const createId = () => {
+    const i = 'id';
+    aux++;
+    return i + aux;
+}
+
 let id = '';
 const createAndOrganizeElements = task => {
+    id = createId();
     // criando 
-    id = gambiarra();
-    console.log(id);
     const divRow = window.document.createElement('div'); 
     divRow.className = 'row';
     const divColTask = window.document.createElement('div'); 
@@ -41,28 +58,29 @@ const createAndOrganizeElements = task => {
     divColBtns.className = 'col';
     const divTask = window.document.createElement('div'); 
     divTask.className = 'task';
+    divTask.id = 'task' + id;
     const divGroupBtns = window.document.createElement('div'); 
     divGroupBtns.className = 'btn-group mr-2';
     divGroupBtns.ariaRoleDescription = 'group';
     divGroupBtns.ariaLabel = 'Segundo grupo';
     const btnCheck = window.document.createElement('button');
-    btnCheck.className = 'btn btn-secondary';
-    btnCheck.id = 'check';
+    btnCheck.className = 'btn btn-secondary check';
+    btnCheck.id = id;
     const btnEdit = window.document.createElement('button');
-    btnEdit.className = 'btn btn-secondary';
-    btnEdit.id = 'edit';
+    btnEdit.className = 'btn btn-secondary edit';
+    btnEdit. id = id;
     const btnDelete = window.document.createElement('button');
-    btnDelete.className = 'btn btn-secondary';
-    btnDelete.id = 'delete';
+    btnDelete.className = 'btn btn-secondary delete';
+    btnDelete.id = id;
     const iconCheck = window.document.createElement('i');
     iconCheck.className = 'fas fa-check';
-    iconCheck.id = 'check';
+    iconCheck.id = id;
     const iconEdit = window.document.createElement('i');
     iconEdit.className = 'fas fa-pencil';
-    iconEdit.id = 'edit';
+    iconEdit.id = id;
     const iconDelete = window.document.createElement('i');
     iconDelete.className = 'fas fa-trash';
-    iconDelete.id = 'delete';
+    iconDelete.id = id;
     // organizando
     divRow.appendChild(divColTask);
     divColTask.appendChild(divTask);
@@ -96,16 +114,14 @@ createNewTask.addEventListener('click', () => {
     if (newTaskName != '') {
         const task = createTask(newTaskName); 
         tasks.push(task);
-        console.log(tasks)
         divTasks.appendChild(createAndOrganizeElements(task));
-
     } else {
         alert('Adicione uma tarefa');
     };
     clearInput(newTaskTxt);
 });
 
-// adicionando o enter
+// adicionando key
 const keyEnter = window.document.getElementById('newTask');
 newTask.addEventListener('keypress', event => {
     if (event.key === 'Enter') {
